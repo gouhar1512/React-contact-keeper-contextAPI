566d41dc2726becd318fdb3046c8f8dbe3b6db7e
gouhar1512 <ushran987@gmail.com>
2020-07-24 21:15:35 +0530

    CRUD completed

--- src/components/contacts/ContactForm.js
+++ src/components/contacts/ContactForm.js
@@ -43,12 +43,7 @@ const ContactForm = () => {
     } else {
       updateContact(contact);
     }
-    setContact({
-      name: "",
-      email: "",
-      phone: "",
-      type: "personal",
-    });
+    clearAll();
   };
 
   const clearAll = () => clearCurrent();
